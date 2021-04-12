dbf028f472f66f3f16f9e9dc08d7eca0e5fc875e
Matt Gotch <mdg0501@gmail.com>
2021-04-12 08:35:03 -0600

    update

--- src/App.js
+++ src/App.js
@@ -2,35 +2,35 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import { API } from 'aws-amplify';
 import {withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
-import { listNotes } from './graphql/queries';
-import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+//import { listNotes } from './graphql/queries';
+//import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
 const initialFormState = { name: '', description: ''}
 
 function App() {
-  const [notes, setNotes] = useState([]);
-  const [formData, setFormData] = useState(initialFormState);
+  //const [notes, setNotes] = useState([]);
+  //const [formData, setFormData] = useState(initialFormState);
 
   useEffect(() => {
     fetchNotes();
   }, []);
 
   async function fetchNotes() {
-    const apiData = await API.graphql({query: listNotes});
-    setNotes(apiData.data.listNotes.items);
+    //const apiData = await API.graphql({query: listNotes});
+    //setNotes(apiData.data.listNotes.items);
   }
 
   async function createNote() {
-    if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createNoteMutation, variables: { input: formData }});
-    setNotes([ ...notes, formData ]);
-    setFormData(initialFormState);
+    //if (!formData.name || !formData.description) return;
+    //await API.graphql({ query: createNoteMutation, variables: { input: formData }});
+    //setNotes([ ...notes, formData ]);
+    //setFormData(initialFormState);
   }
 
   async function deleteNote({id}) {
-    const newNoteArray = notes.filter(note => note.id !== id);
-    setNotes(newNoteArray);
-    await API.graphql({query: deleteNoteMutation, variables: {input: {id}}});
+    //const newNoteArray = notes.filter(note => note.id !== id);
+    //setNotes(newNoteArray);
+    //await API.graphql({query: deleteNoteMutation, variables: {input: {id}}});
   }
 
   return (
@@ -63,4 +63,4 @@ function App() {
   );
 }
 
-export default withAuthenticator(App);
+export default App;
